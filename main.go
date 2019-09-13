b629ba3382319ce4b287dd46f8fdb3c115eb5cee
roz3x <oroz3x@gmail.com>
2019-09-13 19:45:06 -0400

    commented via deploy.sh

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"server/create"
+	"server/friend"
 	"server/posting"
 )
 
 func main() {
-	// port := "8080"
-	// http.HandleFunc("/user/", create.AccountCreator)
-	// http.HandleFunc("/post/", posting.Post)
-	// http.HandleFunc("/frnd/", friend.AddFriend)
-	// http.ListenAndServe(":"+port, nil)
-	fmt.Println(posting.Check("shivang", "ki2"))
+	port := "8080"
+	http.HandleFunc("/user/", create.AccountCreator)
+	http.HandleFunc("/post/", posting.Post)
+	http.HandleFunc("/frnd/", friend.AddFriend)
+	http.ListenAndServe(":"+port, nil)
+	// fmt.Println(posting.Check("shivang", "ki2"))
 }
