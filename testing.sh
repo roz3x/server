ee0abb1b4ec5ac7fbfa80c3d980a2b14f15b7d6c
roz3x <oroz3x@gmail.com>
2019-09-14 09:38:31 -0400

    commented via deploy.sh

--- testing.sh
+++ testing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -rf ./users/*
+# rm -rf ./users/*
 
 
-curl -i localhost:8080/user/peka/123
-curl -i localhost:8080/user/roz3x/157
-curl -i localhost:8080/user/maria/894
+# curl -i localhost:8080/user/peka/123
+# curl -i localhost:8080/user/roz3x/157
+# curl -i localhost:8080/user/maria/894
 
 
-curl -i localhost:8080/frnd/roz3x/peka
-curl -i localhost:8080/fnrd/peka/roz3x
+# curl -i localhost:8080/frnd/roz3x/peka
+# curl -i localhost:8080/fnrd/peka/roz3x
 curl -i localhost:8080/frnd/maria/peka
 
 # curl -i localhost:8080/post/roz3x/peka/hellobrother
