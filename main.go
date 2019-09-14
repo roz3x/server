59233d928dd1c9bfa2ee0dafed122ed4a3164646
roz3x <oroz3x@gmail.com>
2019-09-14 11:49:04 -0400

    commented via deploy.sh

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 	"server/create"
 	"server/friend"
+	"server/home"
 	"server/posting"
 )
 
 func main() {
 	port := "8080"
+	http.HandleFunc("/", home.Home)
 	http.HandleFunc("/user/", create.AccountCreator)
 	http.HandleFunc("/post/", posting.Post)
 	http.HandleFunc("/frnd/", friend.AddFriend)
