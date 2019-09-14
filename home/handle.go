9cb2a4f9c3f979f2b4ec421e367711b962a2d423
roz3x <oroz3x@gmail.com>
2019-09-14 11:56:53 -0400

    commented via deploy.sh

--- home/handle.go
+++ home/handle.go
@@ -2,9 +2,20 @@ package home
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+//Home is home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%v", "hello")
+	pusher, ok := w.(http.Pusher)
+	if !ok {
+		log.Fatal(ok)
+		return
+	}
+	if ok = pusher.Push("./README.md", nil); ok != nil {
+		log.Fatal(ok)
+		return
+	}
 }
